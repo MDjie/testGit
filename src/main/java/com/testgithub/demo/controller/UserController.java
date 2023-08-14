39dabe90e0f1092816c13e9ccddfe98d10993771
MDjie <3228405267@qq.com>
2023-08-14 14:57:27 +0800

    the fundemental structure of the project.

---
+++ src/main/java/com/testgithub/demo/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.testgithub.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/hellouser")
+    public String hellouser(){
+        return "hello My name is DJH!";
+    }
+}
