c95bdeaadadb892e5a05afdeaa567dfb5a1473c9
MDjie <3228405267@qq.com>
2023-08-14 15:06:16 +0800

    fix the bug caused by the version of the spring-boot starter and create the usercontroller

--- src/main/java/com/testgithub/demo/controller/UserController.java
+++ src/main/java/com/testgithub/demo/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     @GetMapping("/hellouser")
-    public String hellouser(){
+    public String Hellouser(){
         return "hello My name is DJH!";
     }
 }
